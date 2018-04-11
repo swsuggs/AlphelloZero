85da2d86b76c66f111efc1ae799812edd33ba624
smoothoperators <davidkartchner@gmail.com>
2018-04-11 13:32:55 -0600

    updated network to improve training

--- alphello_net.py
+++ alphello_net.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from softmax import softmax
-
+import cPickle
 from data_manager import Data_Manager
+from tqdm import trange
 
 class Othello_Network():
 
@@ -22,7 +23,7 @@ class Othello_Network():
         self.conv_size = conv_size
         self.n_res_layers = n_res
         self.regularizer = tf.contrib.layers.l2_regularizer(scale=c)
-        self.dm = Data_Manager(max_size=(board_dim**2 - 4)*1000)  # moves per game TIMES num games to save
+        self.dm = Data_Manager(max_size=(board_dim**2 - 4)*500)  # moves per game TIMES num games to save
 
         # --------------
         # Make Network
@@ -140,7 +141,7 @@ class Othello_Network():
 
     def _value_head(self, input_layer):
         """
-        Estimate value of board state by applying value head network to input_layer.
+        Estimate value of board state by applying vlossalue head network to input_layer.
 
         :param input_layer: the layer to apply value head to
         :return: scalar estimating value of board position (between -1 and 1)
@@ -226,12 +227,15 @@ class Othello_Network():
         losses = []
 
         # sample mini-batch of 2048
-        for i in range(n_iters):
+        print("Training Network")
+        for i in trange(n_iters):
 
             state_batch, pi_batch, z_batch = self.dm.get_batch(batch_size)
             feed_dict = {self.input_layer: state_batch, self.mcts_pi: pi_batch, self.winner_z: z_batch}
-            l, _ = self.sess.run([self.loss, self.optimizer], feed_dict=feed_dict)  # probably don't need to run loss every time
-            losses.append(l)
+            _ = self.sess.run([self.optimizer], feed_dict=feed_dict)
+            if i%2 == 0:
+                l, _ = self.sess.run([self.loss], feed_dict=feed_dict)  # probably don't need to run loss every time
+                losses.append(l)
 
             if verbose:
                 if i % 100 == 0:
@@ -241,3 +245,6 @@ class Othello_Network():
             self.losses.extend(losses)
         else:
             self.losses = losses
+
+        # Save losses to pickle file
+        cPickle.dump(self.losses, open("loss.cpkl",'wb'))
--- train_AI.py
+++ train_AI.py
@@ -4,6 +4,7 @@ from othello import *
 from mcts import MCTS, selfplay, play_game, faceoff
 from alphello_net import Othello_Network as ONET
 from tqdm import tqdm, trange
+import cPickle
 
 
 
@@ -13,8 +14,8 @@ if __name__ == '__main__':
     player = game.player
 
 
-    playing_net = ONET(n_filters=128, n_res=3)
-    training_net = ONET(n_filters=128, n_res=3)
+    playing_net = ONET(n_filters=256, n_res=10)
+    training_net = ONET(n_filters=256, n_res=10)
     # params to manually tune for above nets:
     #   size of game state storage in data manager
     #   scale for weight regularization
@@ -37,13 +38,15 @@ if __name__ == '__main__':
     done = False
     iters = 0
     # while not done:
-    for z in trange(100):
+    new_net_wins = 0
+    trained_net_win_rate = []
+    for z in trange(1000):
         for _ in trange(100):
-
+            tree_depth = 200
             training_mcts = MCTS(1, board, game.player, training_net,c=1)
 
             # Ger states, pis, and zs from a game of self-play
-            states, pis, zs = selfplay(training_mcts,100)
+            states, pis, zs = selfplay(training_mcts,tree_depth)
 
 
             # Having played one game, we can add the game data to the network being trained
@@ -51,30 +54,40 @@ if __name__ == '__main__':
 
             # End game playing loop.
 
-            # Every 10 games, train the neural net for one iteration
-            if iters % 10 == 0 and iters > 0:
-                # Training network can now be trained.
-                n_iters = 1
-                batch_size = 256
-                training_net.train(n_iters, batch_size, verbose=False)
-            iters += 1
+
+        # Training network can now be trained.
+        n_iters = 10
+        batch_size = 512
+        training_net.train(n_iters, batch_size, verbose=False)
+
 
         # play training_net against playing_net for 100 games.
         # If training_net beats playing_net 55% of the time, that is good.
         # Otherwise, that is not good.
         winners = []
         for k in trange(10):
-            if k < 50:
-                winners.append(-play_game(training_net, playing_net, mcts_iters=100))
+            if k < 5:
+                winners.append(-play_game(training_net, playing_net, mcts_iters=tree_depth))
             else:
-                winners.append(play_game(playing_net, training_net, mcts_iters=100))
+                winners.append(play_game(playing_net, training_net, mcts_iters=tree_depth))
 
         # Calculate win rate
         win_rate = np.mean(winners)
+        print(win_rate)
+        print(winners)
+        trained_net_win_rate.append(win_rate)
+        cPickle.dump(trained_net_win_rate, open("win_rates.cpkl",'wb'))
 
-        if win_rate > .55:
+        if win_rate > .1 :
             # copy weights from training_net into playing_net.  The following two lines should be good to go.
             training_net.save_weights()
-            playing_net.restore_weights()
+            playing_net.load_weights()
+            new_net_wins += 1
+            print("New Net win proportion", new_net_wins/(z+1))
+
+        # if win_rate < -.1:
+        #     playing_net.save_weights()
+        #     training_net.load_weights()
+        #     print("Old net is better")
 
     # repeat all of the above from game playing loop.
